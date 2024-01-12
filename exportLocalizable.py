733316db039feda145ec6f93fddb08848679c200
wakaka <wakaka@wakaka.com>
2024-01-12 17:12:56 +0800

    优化导出 Excel

--- exportLocalizable.py
+++ exportLocalizable.py
@@ -21,14 +21,15 @@ TARGET_LOCALIZABLE_DIR = "./result/Localizable"
 
 # 正则匹配 "重新添加" = "<#code#>";
 PATTERN = r'"(?:\\.|[^"\\])*"'
-REPLACE_TEXT = '<#code#>'
+REPLACE_TEXT = "<#code#>"
+KEY_LANG = "zh-Hans"
 
 
 # 单行注释
 def isSignalNote(string):
     if string.startswith("//"):
         return True
-    if string.startswith('#pragma'):
+    if string.startswith("#pragma"):
         return True
     return False
 
@@ -39,18 +40,19 @@ def get_all_lproj_path(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
-            if '.lproj' in file_path:
+            if ".lproj" in file_path:
                 all_files.append(file_path)
 
     return all_files
 
 
 # 取出语言中对应的翻译
-def parse_lproj_file(path):
+# needKey: 是否取出key
+def parse_lproj_file(path, needKey=False):
     print(f"Processing file: {path}")
-    # path: xx/zh-Hans.lproj/Localizable.strings 
+    # path: xx/zh-Hans.lproj/Localizable.strings
     # excel 第一行写入语言类型
-    lproj_name = path.split('/')[-2].split('.')[0]
+    lproj_name = path.split("/")[-2].split(".")[0]
     datas = [lproj_name]
 
     f = open(path)
@@ -69,12 +71,12 @@ def parse_lproj_file(path):
         matches = re.findall(r"\".*?\"", line_text)
         count = len(matches)
         if count > 1:
-            match = matches[1]
+            match = matches[0 if needKey else 1]
             # 还原 && -> \\"
             match = match.replace("&&", '\\"')
             # 去除首尾的双引号
             match = match.strip('"')
-            datas.append(match.replace(REPLACE_TEXT, ''))
+            datas.append(match.replace(REPLACE_TEXT, ""))
     return datas
 
 
@@ -83,15 +85,15 @@ def parse_lproj_file(path):
 def parse_localizable(paths):
     all_datas = []
     for path in paths:
-        datas = parse_lproj_file(path)
-        length = len(datas)
-        if length > 1:
+        if KEY_LANG in path:
             # 中文作为key
-            if 'zh-Hans' in datas[0]:
-                temp = datas.copy()
-                temp[0] = 'key'
-                all_datas.append(temp)
+            temp = parse_lproj_file(path, needKey=True)
+            if len(temp) > 1:
+                temp[0] = "key"
+                all_datas.insert(0, temp)
 
+        datas = parse_lproj_file(path)
+        if len(datas) > 1:
             all_datas.append(datas)
     return all_datas
 
@@ -109,19 +111,19 @@ def write_to_excel(path, data):
 
         # 写入数据到 Excel
         for col_idx, col_data in enumerate(data):
-            col_letter = chr(ord('A') + col_idx)
-            start_cell = f'{col_letter}1'
+            col_letter = chr(ord("A") + col_idx)
+            start_cell = f"{col_letter}1"
             sheet.range(start_cell).options(transpose=True).value = col_data
 
     except Exception as e:
         print(f"发生异常: {e}")
     finally:
         # 保存并关闭 Excel 文件
-        if 'wb' in locals():
+        if "wb" in locals():
             wb.save(path)
             wb.close()
             app.quit()
-            print('结果已保存: ' + path)
+            print("结果已保存: " + path)
 
 
 # 创建xlsx文件
@@ -130,7 +132,7 @@ def create_xlsx(path):
     output_dir = os.path.dirname(path)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-        print('创建目录：' + output_dir)
+        print("创建目录：" + output_dir)
 
     # 创建xlsx文件
     try:
@@ -139,15 +141,15 @@ def create_xlsx(path):
         pass
 
     # 创建一个新的空文件
-    with open(path, 'w'):
+    with open(path, "w"):
         pass
-    print('创建xlsx文件：' + path)
+    print("创建xlsx文件：" + path)
 
 
 def main():
     localizable_path = TARGET_LOCALIZABLE_DIR
     if not os.path.exists(localizable_path):
-        print('TARGET_LOCALIZABLE_DIR 不存在 = ' + localizable_path)
+        print("TARGET_LOCALIZABLE_DIR 不存在 = " + localizable_path)
         sys.exit()
 
     file_path = EXCEL_PATH
@@ -157,5 +159,5 @@ def main():
     write_to_excel(file_path, datas)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
